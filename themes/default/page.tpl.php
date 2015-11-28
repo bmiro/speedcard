208eafe7df99c7e5644a9bd59d9e824cffc2e5f7
Bartomeu Miro <bartomeumiro@gmail.com>
2015-11-28 19:58:19 +0100

    Seem to work

--- themes/default/page.tpl.php
+++ themes/default/page.tpl.php
@@ -1,10 +1,27 @@
-<html>
-<head>
-  <title><?php if ( isset($title) ) { print $title; } ?></title>
-</head>
-<body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title><?php print page_get_title(); ?></title>
 
-<?php print $main; ?>
+    <!-- Bootstrap -->
+    <link href="/static/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/static/css/speedcard.css" rel="stylesheet">
 
-</body>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php print $main; ?>
+
+    <script type="text/javascript" src="/static/js/jquery.min.js"></script>
+    <script type="text/javascript" src="/static/js/bootstrap.min.js"></script>
+  </body>
 </html>
